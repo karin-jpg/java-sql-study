d089f036b0e1bd3da3a995a983c3a1cb3c614836
karin morais <andradkarin@gmail.com>
2023-03-01 20:48:42 -0300

    Added a datasource on ConnectionFactory class

--- src/virtualShop/ConnectionFactory.java
+++ src/virtualShop/ConnectionFactory.java
@@ -4,16 +4,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionFactory {
+import javax.sql.DataSource;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
 	
-	public Connection getConnection() throws SQLException {
+	public DataSource dataSource;
+
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/virtual_shop?useTimezone=true&serverTimezon=UTC");
+		comboPooledDataSource.setUser("virtual_shop_adm");
+		comboPooledDataSource.setPassword("1234");
 		
-		return DriverManager
-				.getConnection("jdbc:mysql://localhost/virtual_shop?useTimezone=true&serverTimezon=UTC", "virtual_shop_adm", "1234");
-		
+		this.dataSource = comboPooledDataSource;
 	}
-	
-	
-	
+
+	public Connection getConnection() throws SQLException {
+
+		return DriverManager.getConnection("jdbc:mysql://localhost/virtual_shop?useTimezone=true&serverTimezon=UTC",
+				"virtual_shop_adm", "1234");
+
+	}
+
 }
