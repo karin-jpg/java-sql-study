6c11528db7e723d28ec9ddfafecfd3244a2c0cd3
karin morais <andradkarin@gmail.com>
2023-03-01 20:19:13 -0300

    Added transactions on insert

--- src/virtualShop/TestInsertWithParams.java
+++ src/virtualShop/TestInsertWithParams.java
@@ -9,29 +9,45 @@ import java.sql.Statement;
 public class TestInsertWithParams {
 
 	public static void main(String[] args) throws SQLException {
-		
-		String name = "Mouse'", description = "salve";
-		
+
 		ConnectionFactory connectionFactory = new ConnectionFactory();
-		Connection connection = connectionFactory.getConnection();
-		
-		PreparedStatement stm = 
-				connection.prepareStatement("INSERT INTO product (name, description) VALUES (? , ? )", Statement.RETURN_GENERATED_KEYS);
-		
+		//adding connections inside try params for them to close automatically
+		try (Connection connection = connectionFactory.getConnection()) {
+
+			connection.setAutoCommit(false);
+			try (PreparedStatement stm = connection.prepareStatement(
+					"INSERT INTO product (name, description) VALUES (? , ? )", Statement.RETURN_GENERATED_KEYS)) {
+
+				addVariable("Tv", "Big screen", stm);
+				addVariable("Radio", "Makes noise", stm);
+				addVariable("Controller", "For your big screen", stm);
+
+				connection.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				connection.rollback();
+
+			}
+		}
+	}
+
+	private static void addVariable(String name, String description, PreparedStatement stm) throws SQLException {
 		stm.setString(1, name);
 		stm.setString(2, description);
-		
+
 		stm.execute();
-		
+
+		if (name == "Radio") {
+			throw new RuntimeException("Error on insert");
+		}
+
 		ResultSet result = stm.getGeneratedKeys();
-		
-		while(result.next()) {
+
+		while (result.next()) {
 			System.out.println("ID criado foi: " + result.getInt(1));
 		}
-		
-		connection.close();
 
-
+		result.close();
 	}
 
 }
