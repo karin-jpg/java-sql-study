c549676c6ec0642ae1eb995864e20228ec19f129
karin morais <andradkarin@gmail.com>
2023-03-01 21:03:55 -0300

    Added a test class for the connection pools

---
+++ src/virtualShop/TestConnectionPool.java
@@ -0,0 +1,16 @@
+package virtualShop;
+
+import java.sql.SQLException;
+
+public class TestConnectionPool {
+
+	public static void main(String[] args) throws SQLException {
+	
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		
+		for (int i = 0; i < 20; i++) {
+			connectionFactory.getConnection();
+			System.out.println("Conexão " + (i+1));
+		}
+	}
+}
