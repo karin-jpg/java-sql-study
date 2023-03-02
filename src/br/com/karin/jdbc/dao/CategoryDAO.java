5d2fe2d5090464701ad3b1c5d0c97c0a93ba6e94
karin morais <andradkarin@gmail.com>
2023-03-02 19:15:48 -0300

    Add function to return categories and it's products

--- src/br/com/karin/jdbc/dao/CategoryDAO.java
+++ src/br/com/karin/jdbc/dao/CategoryDAO.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.karin.jdbc.modelo.Category;
+import br.com.karin.jdbc.modelo.Product;
 
 public class CategoryDAO {
 
@@ -63,5 +64,36 @@ public class CategoryDAO {
 		}
 
 		return categories;
+	}
+	
+	public List<Category> listWithProducts() throws SQLException {
+
+		List<Category> categoriesWithProducts = new ArrayList<Category>();
+		Category last = null;
+		
+		String sql = "SELECT c.id as categoryId, c.name as categoryName,"
+				+ "	p.id as productId, p.name as productName, p.description as productDescription"
+				+ " FROM categories c "
+				+ "	INNER JOIN products p on c.id = p.category_id";
+
+		try (PreparedStatement stm = connection.prepareStatement(sql)) {
+
+			stm.execute();
+
+			try (ResultSet result = stm.getResultSet()) {
+				while (result.next()) {
+					if (last == null || !last.getName().equals(result.getString("categoryName"))) {
+						Category category = new Category(result.getInt("categoryId"), result.getString("categoryName"));
+						last = category;
+						categoriesWithProducts.add(category);	
+					}
+					
+					Product product = new Product(result.getInt("productId"), result.getString("productName"), result.getString("productDescription"));
+					last.addProduct(product);
+				}
+			}
+		}
+
+		return categoriesWithProducts;
 	}
 }
